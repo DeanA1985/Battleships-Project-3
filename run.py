bf936bcd171492b97933bd9c4fcf4aac1da058d2
Dean Akpabio <designersuite@outlook.com>
2024-09-17 00:47:34 +0000

    Defined constants for board representation

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
+import random
+import os 
+
+# Constants for Battleships Board Representation
+MISS_CELL = 'O'
+HIT_CELL = 'X'
+SHIP_CELL ='S'
+EMPTY_CELL = '~'
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
