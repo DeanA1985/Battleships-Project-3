234c878fff7668d64ed0c2889debd14a465fec7f
Dean Akpabio <designersuite@outlook.com>
2024-09-20 09:48:44 +0000

    Simplifying code and syntax

--- run.py
+++ run.py
@@ -2,115 +2,96 @@
 import random
 
 # Constants for Battleships Board Representation
-MISS_CELL = 'O'
-HIT_CELL = 'X'
-SHIP_CELL ='S'
-EMPTY_CELL = '~'
+EMPTY_CELL, SHIP_CELL, HIT_CELL, MISS_CELL = '~','S','X','O'
+
+"""EMPTY_CELL: Represented by '~', indicates a cell that has not been occupied by a ship or hit.
+SHIP_CELL: Represented by 'S', indicates a cell that contains a ship.
+HIT_CELL: Represented by 'X', indicates a cell that has been hit by a player's attack.
+MISS_CELL: Represented by 'O', indicates a cell that has been targeted but does not contain a ship."""
 
 class BattleshipGame:
-
-    """ Main battleships board class. Sets the board size, the number of ships, the players name 
-    and the board type ( player board or the computer) Has methods for adding ships, hits, misses and printing the scores,
-    for each round as well as printing the board when the game is over """
-
     
+    """Initial setup for game, initialises several attributes that manage the games state. These
+    attributes include the players name, the size of the playable grid, the number of ships, two boards
+    one for the player and the computer, it also initialises sets to track hits and misses and a running score
+    for the player"""
+
     def __init__(self):
         self.player_name = ""
         self.grid_size = 0
         self.num_ships = 0
         self.player_board = []
         self.computer_board = []
-        self.hits = set()
-        self.misses = set()
+        self.hits, self.misses = set(), set()
         self.player_score = 0
-        self.computer_score = 0 
-        self.game_over = False 
-
+        
         def initialize_board(self):
-            """Creates an empty grid based on size set by user."""
+            """Creates an empty grid filled with "~" empty cells the size of the grid is determined by
+            the grid size attribute"""
             return [[EMPTY_CELL for _ in range(self.grid_size)] for _ in range(self.grid_size)]
         
         def print_board(self, board, hide_ships=False):
-            """Prints the game board."""
-            for row in board:
-                if hide_ships:
-                    print(''.join([EMPTY_CELL if cell == SHIP_CELL else cell for cell in row]))
-                else:
-                    print('  '.join(row))
-                print()
+            """Print the game board, for printing a game board,
+            which is represented as a 2-dimensional list, 'hide_ships' determines whether the ships should be hidden when 
+            printing the board if hide_ships is set to true the method replaces ship cells with a value of EMPTY cell"""
+            for row in board: print (''.join([EMPTY_CELL if cell == SHIP_CELL and hide_ships else cell for cell in row]))
+            print()
 
         def place_computer_ships(self): 
-            """This places ships randomly on the computer's board (playable grid)"""
+            """This places ships randomly on the computer's board (playable grid)
+            The method uses a loop to place each ship at a random coordinate
+            on the grid until a valid position (an empty cell) is found. 
+            Once a valid position is identified, the ship is placed,
+            and the loop continues until all ships are placed."""
         for _ in range(self.num_ships):
             while True:
-                x = random.randint(0, self.grid_size - 1)
-                y = random.randint(0, self.grid_size - 1)
+                x, y = random.randint(0, self.grid_size - 1), random.randint(0, self.grid_size - 1)
                 if self.computer_board[x][y] == EMPTY_CELL:
+                    
                     self.computer_board[x][y] = SHIP_CELL 
                     break
 
                 def get_user_input(self):
-                    """ Gets the row and column guess from the player with input validation."""
+                    """This method uses input validation to ensure that the entered coordinates
+                    are within the valid range defined by self.grid_size. If the input is invalid,
+                    the user is told to try again until valid coordinates are provided.
+                    The method handles exceptions to catch non-integer inputs like text "abc" and informs
+                    the user accordingly.."""
                     while True:
                         try:
-                            x = int(input(f"Enter row (0 to {self.grid_size - 1}): "))
-                            y = int(input(f"Enter column (0 to {self.grid_size - 1}): "))
-                            if 0 <= x < self.grid_size and 0 <= y < self.grid_size:
+                            x, y = int(input(f"Row (0-{self.grid_size - 1}): ")), int(input(f"Column (0-{self.grid_size - 1}): "))
+                            if 0 <= x < self.grid_size and 0 <= y < self.grid_size: 
                                 return x, y 
-                            else:
-                                print("These coordinates are off the grid. Please try again.")
+                            print("Coordinates are off the grid. Try Again.")
                         except ValueError:
-                            print("Please enter valid integers.")
+                            print("Invalid input. Please enter integers.")
 
-                            def make_guess(self, x, y):
-                                """ When the player makes a guess the result is updated on the board"""
-                                if (x, y) in self.hits or (x, y) in self.misses:
-                                    print("This spot has been guessed already!")
-                                    return False
-
-                                    if self.computer_board[x][y]== SHIP_CELL:
-                                        print("Hit!!!")
-                                        self.player_board[x][y] = HIT_CELL
-                                        self.hits.add((x, y))
-                                        return True
-                                    else:
-                                        print("Miss!!!")
-                                        self.player_board[x][y] = MISS_CELL
-                                        self.misses.add((x, y))
-                                        return False
-
-                            def check_win_condition(self):
-                                """This function checks to see if all the ships have been hit."""
-                                return len(self.hits) == self.num_ships
-
-                            def play_round(self):
-                                """Play one round of the game."""
-                            print(f"\n{self.player_name}'s board:")
-
-                            self.print_board(self.computer_board, hide_ships=True)
- 
-                            x, y = self.get_user_input()
- 
-                            if self.make_guess(x, y):
-                                self.player_score += 1
-                            else:
-                                self.player_score -= 1
- 
-                            # Shows the updated player board
-                            print(f"\n{self.player_name}'s updated board:")
-
-                            self.print_board(self.player_board)
-
-                            def ask_play_again(self):
-                                        """Ask the player if they would like to play again"""
-                            while True:
-                                        answer = input("Woulld you like to play another game? (yes/no):").lower()
-                                        if answer == 'yes':
-                                                return True
-                                        elif answer == 'no':
-                                                return False
-                                        else:
-                                                print("Please enter 'yes' or 'no'.")
+                def make_guess(self, x, y):
+                    """Player makes a guess and the result is updated on the board
+                    it checks to see if coordinates have been guessed and prevents you
+                    from making repeat guesses"""
+                    
+                    if (x, y) in self.hits or (x, y) in self.misses:
+                        print("You have already guessed this spot!")
+                        return False
+                    if self.computer_board[x][y] == SHIP_CELL: print("Hit!") 
+                    self.player_board[x][y] = HIT_CELL
+                    self.hits.add((x, y)) 
+            else:
+                    print("Miss!")
+                    self.player_board[x][y]= MISS_CELL
+                    self.misses.add((x, y))
+                    return True
+                                   
+            def play_round(self):
+             """Play one round of the game."""
+             self.print_board(self.player_board)
+             
+             self.print_board(self.computer_board, hide_ships=True)
+             x, y = self.get_user_input()
+             self.make_guess(x ,y)
+             
+            
 
                             def setup_game(self):
                                         """Setup the game grid and number of ships"""
@@ -190,6 +171,9 @@ class BattleshipGame:
 
                                                             def main():
                                                                 """Main function to start the game."""
+                                                                
+                                                                print("Game Started!")
+                                                                
                                                                 print_game_instructions() 
 
                                                                 game = BattleshipGame()
