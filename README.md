505682c6d62c58b9aa76f9deaff0ec9e0c6267ad
Dean Akpabio <designersuite@outlook.com>
2024-09-22 00:43:56 +0000

    Refactor code

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+!
+# Battleship Game
+This is a terminal based version of the classic Battleship game where you play against the computer. You will set up a custom grid and choose the number of ships to place on the grid. The objective all the game is to sink all of the computer's ships by guessing their locations in the grid.
 
-Welcome,
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **May 14, 2024**
+## Description
+Your goal is to sink all of the computer's ships by guessing their positions. The game is fully customizable, allowing you to set up the grid size and the number if ships that you want to place on the board. The game incorporates error handling for invalid inputs like placing text where there should be numbers. 
 
-## Reminders
+## Games Rules
+ - The game is played on a square grid where you choose the size (from 5x5 to 10x10).
+- On each turn you are asked to guess a location row and column to strike.
+- The game will tell you if you hit or miss a ship
+- The goal is to sink all of the computer's ships by hitting all of their positions 
+- After sinking all of the ships the game is complete.
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+## Features
+- Customisable Grid : Choose the size of the game board (5x5 to 10x10).
+- Number of Ships : You can decide how many ships you wish to place on the board but the only parameters are the board size.
+- PEP8 Compliant: The game is 
 
-## Creating the Heroku app
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
 
 1. `heroku/python`
 2. `heroku/nodejs`
--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import os
 import random
+import sys
 
 # Constants for Battleships Board Representation
 EMPTY_CELL, SHIP_CELL, HIT_CELL, MISS_CELL = "~", "S", "X", "O"
 
 """This class is designed to initialize various attributes related to the game,
 including player information, board sizes, and tracking hits and misses as well
-as an integer to keep track of the players score"""
+handling the players and computer turns and scores"""
 
 
 class BattleshipGame:
@@ -16,37 +17,42 @@ class BattleshipGame:
         self.num_ships = 0
         self.player_board = []
         self.computer_board = []
+        self.player_ships = set()
         self.hits, self.misses = set(), set()
-        self.player_score = 0
+        self.computer_hits, self.computer_misses = set(), set()
 
     """Creates an empty grid. The method relies on two attributes:
     self.grid_size, which creates the dimensions of the grid, and
-    EMPTY_CELL, which gives the value of each cell on same grid"""
+    EMPTY_CELL, which gives the value of empty to each cell on grid"""
 
     def initialize_board(self):
         return [
-            [EMPTY_CELL for _ in range(self.grid_size)] for _ in range
-            (self.grid_size)
+           [EMPTY_CELL for _ in range(self.grid_size)] for _ in
+           range(self.grid_size)
         ]
 
-    """Prints the game board"""
+    """Prints the game board, If hide_ships is True, it will hide the ships
+    This can be used to show the player's progress on the computer's board"""
 
     def print_board(self, board, hide_ships=False):
         for row in board:
             print(
                 " ".join(
                     [
-                        EMPTY_CELL if cell == SHIP_CELL and hide_ships
-                        else cell
+                        HIT_CELL if cell == HIT_CELL else
+                        MISS_CELL if cell == MISS_CELL else
+                        EMPTY_CELL if hide_ships and cell == SHIP_CELL else
+                        cell
                         for cell in row
                     ]
                 )
             )
         print()
 
-    """Randomly places ships on the computers board, using a while loop
-    to randomly generates coordinates until the required number of ships set
-    by self.num_ships is met it places them within the bounds of the board"""
+    """Randomly places ships on the computers and players board, using a while
+    loop to randomly generates coordinates until the required number of ships
+    set by self.num_ships is met it places them within the bounds of the
+    board"""
 
     def place_computer_ships(self):
         ships_placed = 0
@@ -58,6 +64,19 @@ class BattleshipGame:
             """Ensure the spot is empty before placing a ship"""
             if self.computer_board[x][y] == EMPTY_CELL:
                 self.computer_board[x][y] = SHIP_CELL
+                ships_placed += 1
+
+    def place_player_ships(self):
+        ships_placed = 0
+        while ships_placed < self.num_ships:
+            """This generates random coordinates for ship placement"""
+            x = random.randint(0, self.grid_size - 1)
+            y = random.randint(0, self.grid_size - 1)
+
+            """Ensure the spot is empty before placing a ship"""
+            if self.player_board[x][y] == EMPTY_CELL:
+                self.player_board[x][y] = SHIP_CELL
+                self.player_ships.add((x, y))
                 ships_placed += 1
 
     """Gets row and column guess from the player using input
@@ -84,21 +103,58 @@ class BattleshipGame:
 
         if self.computer_board[x][y] == SHIP_CELL:
             print("Hit!")
-            self.player_board[x][y] = HIT_CELL
+            self.computer_board[x][y] = HIT_CELL
             self.hits.add((x, y))
         else:
             print("Miss!")
-            self.player_board[x][y] = MISS_CELL
+            self.computer_board[x][y] = MISS_CELL
             self.misses.add((x, y))
 
+        return True
+
+    """Computer makes random guess on the player's board"""
+
+    def computer_guess(self):
+        while True:
+            x = random.randint(0, self.grid_size - 1)
+            y = random.randint(0, self.grid_size - 1)
+            if (x, y) not in self.computer_hits and (x, y) not in self.computer_misses:
+                if (x, y) in self.player_ships:
+                    print(f"Computer hit your ship at ({x},{y})!")
+                    self.computer_hits.add((x, y))
+                    self.player_board[x][y] = HIT_CELL
+                else:
+                    print(f"Computer missed at ({x}, {y}).")
+                    self.computer_misses.add((x, y))
+                    self.player_board[x][y] = MISS_CELL
+                break
+
+    """Checks to see if all computers ships have been sunk
+    if there is any ship left the game continues"""
+
+    def all_computer_ships_sunk(self):
+        for row in self.computer_board:
+            if SHIP_CELL in row:
+                return False
+        return True
+
+    """Checks to see if all players ships have been sunk
+    if there is any ship left the game continues"""
+
+    def all_player_ships_sunk(self):
+        for row in self.player_board:
+            if SHIP_CELL in row:
+                return False
         return True
 
     """Play one round of the game. The method includes: Printing the player's
     board.Printing the computer's board with the ships hidden. Getting user
     input for the coordinates of the guess. Making a guess based on the
-    provided coordinates x & y row and column."""
+    provided coordinates x & y row and column. The Player and Computer
+    take turns"""
 
     def play_round(self):
+        # Player's turn
         print(f"\n{self.player_name}'s board:")
         self.print_board(self.player_board)
         print("\nComputer's board (your progress):")
@@ -106,9 +162,13 @@ class BattleshipGame:
         x, y = self.get_user_input()
         self.make_guess(x, y)
 
+        # Computer's turn
+        print("\nComputer's turn...")
+        self.computer_guess()
+
     """Setup_game prompts the user to input the grid size and the number
     of ships, initializes two game boards (one for the player and one for
-    the computer), and places ships on the computer's board"""
+    the computer), and places ships on the computer and players board"""
 
     def setup_game(self):
         self.grid_size = int(input("Enter the grid size (5-10): "))
@@ -117,34 +177,35 @@ class BattleshipGame:
         self.player_board = self.initialize_board()
         self.computer_board = self.initialize_board()
         self.place_computer_ships()
+        self.place_player_ships()
 
     """Main game loops until all the ships are sunk"""
-
     def play(self):
-        while len(self.hits) < self.num_ships:
+        while not self.all_computer_ships_sunk() and not self.all_player_ships_sunk():
             self.play_round()
-        print(f"Well Done {self.player_name}, you sunk all the ships!")
+            if self.all_computer_ships_sunk():
+                print(f"All computer ships sunk! {self.player_name}, wins!")
+            else:
+                print("The computer has sunk all your ships. Game over!")
 
+    """Prints the games instructions"""
 
-"""Prints the games instructions"""
-
-
-def print_game_instructions():
-    print("Welcome to Battleships!\n")
-    print("Instructions:")
-    print("1. Play on a custom grid")
-    print(
-        "2. Guess the location of the "
-        "computer's ships by entering row/column values."
-    )
-    print("3. Hits ('X'), Misses ('O'). Sink all ships to win.\n")
+    def print_game_instructions():
+        print("Welcome to Battleships!\n")
+        print("Instructions:")
+        print("1. Play on a custom grid")
+        print(
+            "2. Guess the location of the "
+            "computer's ships by entering row/column values."
+        )
+        print("3. Hits ('X'), Misses ('O'). Sink all ships to win.\n")
 
 
 """Main function to start the game"""
 
 
 def main():
-    print_game_instructions()
+    BattleshipGame.print_game_instructions()
     game = BattleshipGame()
     game.player_name = input("Please enter your name: ")
     game.setup_game()
